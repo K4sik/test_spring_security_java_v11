1c64425d51dad7ea5942d1d6165901c8522b2f54
Roman Kasarab <romankasarab85@gmail.com>
2020-09-11 13:53:27 +0300

    Initial commit

---
+++ src/main/java/com/kas/test_spring_security_java_v11/TestSpringSecurityJavaV11Application.java
@@ -0,0 +1,13 @@
+package com.kas.test_spring_security_java_v11;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TestSpringSecurityJavaV11Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(TestSpringSecurityJavaV11Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/kas/test_spring_security_java_v11/TestSpringSecurityJavaV11ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.kas.test_spring_security_java_v11;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TestSpringSecurityJavaV11ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
